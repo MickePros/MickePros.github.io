390385a9784a23d1112fb4b5ac7747e5136201d4
MickePros <Mikael_Pros@hotmail.com>
2023-06-06 12:29:09 +0200

    add simple sentence arrays and bare bones function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello, World!');
+const male = ['Max','Alex','John','Kyle','Sam'];
+const verb = ['is dancing with', 'is baking with', 'is running with', 'is joking with', 'is swimming with'];
+const female = ['Tina','Dora','Maria','Freya','Lisa'];
+
+const randomMessage = (male, verb, female) => {
+    return 'test';
+}
+
+console.log(randomMessage(male,verb,female));
